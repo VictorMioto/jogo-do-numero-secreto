1d68f3cbefba09edb0e420d6bac172b25005f8ac
Victor Mioto <victor.amioto@gmail.com>
2025-12-04 17:13:47 -0300

    alterando limite para 100 de novo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 50;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,13 +11,13 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemIncicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemIncicial();
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
